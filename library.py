82000e465518f664f410aba878205c757dee95c1
Scabbard <saransh2599@gmail.com>
2019-10-16 12:07:37 +0530

    Additional fuctions for library.py

--- library.py
+++ library.py
@@ -49,6 +49,24 @@ class Hand:
         filtered.sort(key=lambda point: point[1])
         return [pt for idx, pt in zip(range(5), filtered)]
 
-    
+    def filter_points(self, points, filter_value):
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                if points[i] and points[j] and self.dist(points[i], points[j]) < filter_value:
+                    points[j] = None
+        filtered = []
+        for point in points:
+            if point is not None:
+                filtered.append(point)
+        return filtered
 
-   
+    def get_center_of_mass(self):
+        if len(self.contours) == 0:
+            return None
+        M = cv2.moments(self.contours)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        return (cX, cY)
+
+    def dist(self, a, b):
+        return math.sqrt((a[0] - b[0])**2 + (b[1] - a[1])**2)
